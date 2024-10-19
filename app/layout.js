08fc1db39fb61fd8b8176f88e991bc2874247791
Barış Kahraman <baris-kahra@hotmail.com>
2024-10-19 13:17:37 +0300

    Başlangıç değerlerine dönüldü.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import localFont from "next/font/local";
-import "./globals.css";
+import Sidebar from "@/components/sidebar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Sidebar />
         {children}
       </body>
     </html>
---
+++ components/sidebar/index.jsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Sidebar(){
+  return(
+    <Image src={"/logo.png"} width={150} height={150} />
+  )
+}
